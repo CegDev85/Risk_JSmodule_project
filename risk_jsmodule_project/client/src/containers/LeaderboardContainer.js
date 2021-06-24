7264bdb426b10910ae644d99e57ad9af2a0db868
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-06-24 19:58:53 +0100

    containers

---
+++ risk_jsmodule_project/client/src/containers/LeaderboardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeaderboardContainer = () => {
+    return (
+        <div>
+            <h2>Evening</h2>
+        </div>
+    )
+}
+
+export default LeaderboardContainer
---
+++ risk_jsmodule_project/client/src/containers/WelcomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomeContainer = () => {
+    return (
+        <div>
+            <h2>Planet Earth</h2>
+            
+        </div>
+    )
+}
+
+export default WelcomeContainer
