8b6f0ad5220281419d7bc281b3ab7b798989a567
jandrewgordon <jandrewgordon@gmail.com>
2021-06-26 13:21:04 +0100

    pre-pull commit

--- risk_jsmodule_project/client/src/components/GameMap.js
+++ risk_jsmodule_project/client/src/components/GameMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { VectorMap } from '@south-paw/react-vector-maps';
 import usa from '../resources/usa.json'
 
@@ -6,7 +6,19 @@ const GameMap = ({players}) => {
   
     const [hovered, setHovered] = React.useState('None');
     const [selectedTerritory, setSelectedTerritory] = React.useState('None');
-  
+    // const [mapData, setMapData] = useState([])
+
+    // useEffect(() => {
+    //   getMapData()
+    //   console.log(mapData)
+    // }, [])
+
+    // const getMapData = function(){
+    //   fetch('../resources/states.json')
+    //   .then(res=>res.json())
+    //   .then(mapData => setMapData(mapData))
+    // }
+
     const layerProps = {
       onClick: ({ target }) => {
         setSelectedTerritory({
@@ -15,12 +27,12 @@ const GameMap = ({players}) => {
           "troops": target.attributes.troops.value,
           "borders": target.attributes.borders.value
         })
-        console.log(target.attributes.borders.value);
-        getBorders(target.attributes.borders.value);
+        const borders = getBorders(target.attributes.borders.value);
+        // highlightBorders(borders);
       },
     };
 
-    // Seperate the borders into seperate strings
+    // Returns an array of bordering state names
     const getBorders = function(string) {      
       let bordersList = [];
       let index = 0;
@@ -28,7 +40,6 @@ const GameMap = ({players}) => {
       let tempString = string;
       console.log(tempString)
       for(let char of string){
-        console.log(char)
         index += 1;
         if(char === ','){
           bordersList.push(tempString.slice(newWord, index-1));
@@ -40,13 +51,19 @@ const GameMap = ({players}) => {
           bordersList.push(tempString.slice(newWord, index))
         }
       }
-      console.log(bordersList)
       return bordersList;
     }
 
-    
-
-
+    // Highlight bordering states on click
+    const highlightBorders = function(borders){
+      for(let borderName of borders){
+        for(let state of usa){
+          if(borderName === state.name){
+            console.log(borderName)
+          }
+        }
+      }
+    }
 
     return (
       <div>
